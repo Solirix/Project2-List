2968705bf9a72aee586c8c88529bd79dbc2aa8d0
Solirix <99198915+Solirix@users.noreply.github.com>
2022-09-26 17:07:18 +0000

    Create lststack.h

---
+++ lststack.h
@@ -0,0 +1,28 @@
+/* Filename:  lststack.h
+
+   Programmer:  Br. David Carlson
+
+   Date:  March 28, 1998
+   
+   Revised:  March 7, 2014 to not use an abstract base class.
+
+   This is the header file to accompany LstStack.cpp.  They provide the
+   LstStackClass, a list-based implementation of a stack.
+*/
+
+#include "list.h"
+
+
+class LstStackClass
+   {
+   public:
+      // No constructor is mentioned here.  We instead use the default
+      // constructor automatically supplied by the compiler.
+      bool Empty(void) const;
+      void Push(const ItemType & Item);
+      void Pop(ItemType & Item);
+   private:
+      ListClass List;   // an embedded List object
+   };
+
+
