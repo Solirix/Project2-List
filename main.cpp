f36923800e1ca06a807a3f65fb80fdd6fedbde50
Solirix <hades5132@gmail.com>
2022-10-10 16:46:43 -0400

    new build with comments. Finished!

--- main.cpp
+++ main.cpp
@@ -14,71 +14,57 @@
 
 #include "lststack.hpp"
 
-
+//Given: a remainder greater than 9
+//Task: convert that number to it's hex value
+//Return: alphanumeric character associated with that value
 char convertToHex(int numToConvert);
+
+//Given: initial quotient
+//Task: calculate the modulo 16 to find the remainder
+//Return: the remainder after division and subtraction
 int calculateRemainder(int parentQuotient);
+
+//Given: nothing
+//Task: Print the stack and determine if a remainder needs converted
+//Return: the list of remainders in reverse order
 void popAndPrint(LstStackClass& Stack);
 
 int main(void) {
    LstStackClass Stack; 
    int userNum = 0;
-   int remainder = 0;
+   int remainder = 0;                                   // variable initialization
    int quotient, nextQuotient = 0;
-
-/*
-getting remainders, putting them on the stack, then popping them off the stack
-
-user enters any numeber, you divide that mumber by 16 to convert to hex, until you get a quotient of 0. THat is when it stops
-
-ex.
- 3470     SECRET: JUST DO 3470 mod 16 to get 14!! the remainder!!! (don't forget the next step is 216 mod 16... so on)
- divide by 16
- rounds down to 216
- now take 216, and multiply it by 16 since you divided by 16
- 3470 minus ANSWER^ to get remainder
- 14 is remainder for this one. this gets pushed onto the stack - nothing else is done with it
- 
-   now take 216, divide by 16..
-
-   do this repeatedly until it rounds down to 0, finish it, and do nothing after. that's it!
-
-   now just pop everything off the stack!
-
-
-   NOTE: in hex, 0 through 9 is just the number, but 10 is A, 11 is B, so on... CONVERT ANY DOUBLE DIGIT NUMS TO HEX BEFORE DISPLAYING (all on one line too)
-
-when quotient is 0, remiander ends up being 13.. so most recent stack push should be 13 when finished
-*/
 
 
    for (int i = 0; i >= 0; ++i) {  // infinite loop (break is applied if user enters 0)
-      std::cout << "Please enter a positive base 10 integer (or 0 to end the program)\n";
-      std::cin >> userNum;
+      std::cout << "Please enter a positive base 10 integer (or 0 to end the program)\n"; // prompt
+      std::cin >> userNum;    
 
          if (userNum == 0) {  // checks for userinput of 0
             break;           // breaks out of the loop if true
          }
-         else if (userNum < 0) {
+         else if (userNum < 0) {       // Checking for negative input and ending the program if true
             std::cout << "Please do not enter a negative integer.\n";
-            break;
+            std::cout << "The program will now end\n";
+            break;                  // breaks if the user entered a negative integer
          }
          else {
-            quotient = userNum;
+            quotient = userNum;             // if the user entered a positive, base 10 integer, set quotient equal to userNum (for easier loop usage later)
          }
 
 
       do {
 
-         remainder = calculateRemainder(quotient);
+         remainder = calculateRemainder(quotient);          
          
-         Stack.Push(remainder);
+         Stack.Push(remainder);       // push the current remainder onto the stack
 
          nextQuotient = quotient / 16;
-         quotient = nextQuotient;
+         quotient = nextQuotient;        //preparing for the next iteration of the loop
 
-      } while (quotient != 0);
+      } while (quotient != 0);   // continue until the quotient becomes 0
 
-      popAndPrint(Stack);
+      popAndPrint(Stack);    // output everything in the stack
    }
 
 
@@ -105,7 +91,7 @@ char convertToHex(int numToConvert) {
       return 'F';
    }
    else {
-      std::cout << "ERROR - Remainder is not within the 0-15 range to be converted\n";
+      std::cout << "ERROR - Remainder is not within the 0-15 range to be converted\n";  // case just in case an integer ends up incorrect
       return 'Z';
    }
 }
@@ -131,18 +117,15 @@ void popAndPrint(LstStackClass& Stack) {
    char remainderAsHex = 'A'; 
    std::cout << "All remainders as hex: \n";
 
-   while (Stack.Empty() != true) {
-      Stack.Pop(Item);
+   while (Stack.Empty() != true) {        // continue until the stack is empty
+      Stack.Pop(Item);        // pop the most previous item to convert or display
       
-      if (Item > 9) {
+      if (Item > 9) {       // convert the item to hex if needed
          remainderAsHex = convertToHex(Item);
          std::cout << remainderAsHex << endl;
       }
-      else {
+      else {              // unless it isn't needed, then just output it
          std::cout << Item << endl;
       }
    }
-   
-   
-   
 }
