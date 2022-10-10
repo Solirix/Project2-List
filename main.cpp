5effdd836d7fdd84d8e1fcc4311ea7bc7d37957e
Solirix <hades5132@gmail.com>
2022-10-10 13:23:44 -0400

    function mapping... problems with using POP as of now

--- main.cpp
+++ main.cpp
@@ -16,11 +16,13 @@
 
 
 char convertToHex(int numToConvert);
+int calculateRemainder(int parentQuotient);
 
 int main(void) {
    LstStackClass Stack; 
    int userNum = 0;
    int remainder = 0;
+   char remainderAsHex = 'A';
 
 /*
 getting remainders, putting them on the stack, then popping them off the stack
@@ -32,7 +34,7 @@ ex.
  divide by 16
  rounds down to 216
  now take 216, and multiply it by 16 since you divided by 16
- 3470 - ANSWER to get remainder
+ 3470 minus ANSWER^ to get remainder
  14 is remainder for this one. this gets pushed onto the stack - nothing else is done with it
  
    now take 216, divide by 16..
@@ -48,13 +50,41 @@ when quotient is 0, remiander ends up being 13.. so most recent stack push shoul
 */
 
 
-   for (int i = 0; i >= 0; ++i) {
+   for (int i = 0; i >= 0; ++i) {  // infinite loop (break is applied if user enters 0)
       std::cout << "Please enter a positive base 10 integer (or 0 to end the program)\n";
-
       std::cin >> userNum;
 
-      remainder = userNum % 16;
+      if (userNum == 0) {  // checks for userinput of 0
+         break;           // breaks out of the loop if true
+      }
+      else if (userNum < 0) {
+         std::cout << "Please do not enter a negative integer.\n";
+         break;
+      }
 
+      remainder = calculateRemainder(userNum);
+      std::cout << "The remainder is " << remainder << endl;
+
+      if (remainder > 9) {
+         remainderAsHex = convertToHex(remainder);
+         Stack.Push(remainderAsHex);
+
+         std::cout << remainder << " converted to hex is " << remainderAsHex << endl;
+      }
+      else {
+         Stack.Push(remainder);
+      }
+   }
+
+
+
+
+   if (Stack.Empty() == true) {
+      std::cout << "No remainder calculated!\n";
+   }
+   else {
+      std::cout << "All remainders as hex: \n";
+      Stack.Pop();
    }
 
 
@@ -86,4 +116,20 @@ char convertToHex(int numToConvert) {
       std::cout << "ERROR - Remainder is not within the 0-15 range to be converted\n";
       return 'Z';
    }
+}
+
+int calculateRemainder(int parentQuotient) {
+   int remainder = 0;
+
+   remainder = parentQuotient % 16; // calculate the remainder of the user input 
+      /*
+      This does 
+
+      parentQuotient / 16 = X
+      parentQuotient - X = remainder
+      
+      and this works because of the way integer division rounds down
+      */
+
+     return remainder;
 }
